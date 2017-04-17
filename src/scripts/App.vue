9f394bd175e46daf40ea7e4d60ab61c68557455e
yuhao.ju <yuhao.ju@qunar.com>
2017-04-17 14:43:27 +0800

    init

---
+++ src/scripts/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+        <img src="http://oji8kngu4.bkt.clouddn.com/vue.png">
+        <h1>ykit-seed-vue</h1>
+        <ul>
+            <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+            <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
+            <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'app',
+    data() {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
---
+++ ykit.js
@@ -0,0 +1,14 @@
+module.exports = {
+    plugins: ['vue'],
+    config: {
+        export: [
+            './scripts/main.js', './styles/main.css'
+        ],
+        modifyWebpackConfig: function(baseConfig) {
+            // edit ykit's Webpack configs
+            return baseConfig;
+        }
+    },
+    hooks: {},
+    commands: []
+};
